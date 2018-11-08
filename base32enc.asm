76e031d093c1f895ed9c3284927fbdc3b49e4cb2
Beastlike3773 <manuel.gasser.ch@gmail.com>
2018-11-08 14:14:04 +0100

    Started refactoring for encoding

--- base32enc.asm
+++ base32enc.asm
@@ -16,11 +16,11 @@ CTable:
 section .bss
 
 	; Buffer for input
-	BUFFLEN	equ 16			; We read the file 16 bytes at a time
+	BUFFLEN	equ 32			; We read the file 16 bytes at a time
 	Buff: resb BUFFLEN		; Text buffer itself
 
 	; String for output
-	STRLEN	equ 32
+	STRLEN	equ 64
 	Str: resb STRLEN
 
 ; Text section
@@ -29,74 +29,16 @@ section .text
 global _start
 
 _start:
-	nop						; This no-op keeps gdb happy...
-	call Read				; Call read function
-	call Convert			; Call convert function --> change name and comment
-	call Print
-	jmp Exit
 
 
-Read:
-	mov RAX, 3				; Specify sys_read call --> to change
-	mov RDI, 0				; Specify File Descriptor 0: Standard Input --> to change
-	mov RCX, Buff			; Move buff into 
-	mov RDX, BUFFLEN		; Move size of Buff into RBX
-	int	80h					; make Syscall --> to change
-
-	mov RBP, RAX			; Save # of bytes read from file for later --> to change
-
-; Number of bits read from input
-	mov	RAX, RBP
-	mov RBX, 8
-	mul RBX
-	mov	RBX, 5
-	div	RBX
-	cmp RDX, 0				; Check if RAX % RBX = 0 --> to change
-	jne False
-
-True:
-	mov	R10, RAX
-
-False:
-	add RAX, 1
-	mov	R10, RAX
-
-	cmp RBP, 0				; If eax=0, sys_read reached EOF on stdin --> to change
-	je Exit					; Jump If Equal (to 0, from compare) --> to change
-	ret						; return
-
-Convert:
-	mov	RCX, 0				; Move 0 to the CL register for a counter
-	mov	R9, 0				 
-	xor RBX, RBX			; Reset the RBX register
-
-.loop:
-	mov	BL, byte [Buff+RCX]	; Get the first byte from the buff
-	ror	RBX, 8				; Rotato Potato -> plz change dude
-	inc	RCX					; Increment counter
-	cmp	RCX, 5				; Repeat 5 times to get 5 bytes so it can be divided by 5
-	jne	.loop
-
-	shr RBX, 24
-	xor	RCX, RCX
-	mov	RAX, RBX
-	and	RAX, 1Fh
-	mov	DL, byte [CTable+RAX]
-	mov byte [Str+R9], DL
-	inc	R9
-
-	; insert test if finished
-	cmp R10, R9
-	jne .loop
-	ret
-
-Print:
-	mov	RAX, 1
+ReadBuff:
+	mov	RAX, 3			; Get input from user
 	mov	RDI, 0
-	mov	RSI, Str
-	mov	RDX, STRLEN
-	syscall
-	ret
+	mov	RCX, Buff		; Write memory address from buff
+	mov	RDX, BUFFLEN	; Length that should be read
+	int 80h				; Make kernel call
+
+
 
 
 Exit:
