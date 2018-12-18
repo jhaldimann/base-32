460f104508f1612e976636d39e785de51e94858d
jhaldimann <julian@praxishaldimann.ch>
2018-12-18 22:43:24 +0100

    Refactoring and comments

--- base32dec.asm
+++ base32dec.asm
@@ -4,48 +4,41 @@
 ; Last updated	: 18/12/2018
 ; Description	: Small assembly program to decode data with base32
 
-; Data section
-SECTION .bss					; Section of uninitialised data
-	BUFFLEN	equ 1				; read the input 1 byte
-	Buff: resb BUFFLEN			; Text buffer
-	RESULT equ 1				; result for translated 5bit pairs
-	Char: resb RESULT			; length of result
+SECTION .bss					
+	BUFFLEN	equ 1				; Just read one byte
+	Buff: resb BUFFLEN			; Define here the Buffer
+	RESULT equ 1				; Define here the result
+	Char: resb RESULT			; Define how long the result should be
 	
 SECTION .data
-	; reverse table to get 5 bit value from ASCII value -50 as offset
 	Table: 	db 1Ah,1Bh,1Ch,1Dh,1Eh,1Fh,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h
 			db 01h,02h,03h,04h,05h,06h,07h,08h,09h,0Ah,0Bh,0Ch,0Dh,0Eh,0Fh,10h
 			db 11h,12h,13h,14h,15h,16h,17h,18h,19h,00h,00h,00h,00h,00h,00h,00h
 	
-SECTION .text		; Section of code
+SECTION .text
 	
 GLOBAL 	_start
 
 _start:
 	
 Read:
-	mov RAX, Buff				; Offset of Buffer to RAX
-	mov RBX, BUFFLEN			; Number of bytes to read to RBX
-	call ReadBuff				; Call ReadBuff
-	mov RBP, RAX				; Save # of bytes read from file
-	cmp RBP, 0					; If RBP=0, sys_read reached EOF on stdin	
-	je Exit						; If RBP is equal to 0 jump to exit (nothing todo here)
-	mov DL, byte [Buff]			; get the read byte to RDX		
-	cmp DL, 0ah					; or LF (CRLF is a new line, skip that)
-	je Read						; Read the next byte
-	cmp DL, "="					; if its a "="
-	jne NotEqual
-	inc R8						; Increment the counter by 1
-	jmp IsEqual					; Jump to the function ...
+	mov RAX, Buff				; Move the buffer content (user input) to RAX 		
+	mov RBX, BUFFLEN			; Move the length of the buffer to the RBX
+	call ReadBuff				; Call the ReadBuff function
+	mov RBP, RAX				; Move the buffer content into the RBX register
+	cmp RBP, 0					; Check if the buffer is 0
+	je Exit						; If buffer is empty jump to the Exit function
+	mov DL, byte [Buff]			; Move one byte of the buffer into the DL register
+	cmp DL, 0Ah					; Compare the byte with a EOL
+	je Read						; If the byte was an EOL continue with loop (skip this type)
+	cmp DL, "="					; Compare the read byte with a "="
+	jne NotEqual				; If the read byte is not a = jump to the NotEqual function
+	inc R8						; Increment the R8 by 1
+	jmp IsEqual					; Else jump to the isEqual method
 
+; Read the input of the user
 ReadBuff:
-	; Input:
-	;  RAX -> where to read
-	;  RBX -> number of characters to read
-	; Output:
-	;  RAX -> number of read input
-
-	; Push everything because of the syscall
+	; Push everything because of the syscall (user input)
 	push RCX					; Save the RCX value in the stack
 	push RDX					; Save the RDX value in the stack
 	push RSI					; Save the RSI value in the stack
@@ -53,9 +46,11 @@ ReadBuff:
 	push R8						; Save the R8 Value in the stack
 	mov RSI,RAX					; Move the input to the RSI register
 	mov RDX,RBX					; Move the lenght of the input to the RDX register
-	mov RAX,0					; Specify sys_read call
-	mov RDI,0					; Specify File Descriptor 0: Standard Input
-	syscall
+	mov RDI,0					; Set RDI to 0 (for user input)
+	mov RAX,0					; Set RAX to 0
+
+	syscall						; Get input of user here 
+
 	; Now you can get everything back from the stack
 	pop R8						; Add the value back to the R8 register
 	pop RDI						; Add the value back to the RDI register
@@ -65,97 +60,90 @@ ReadBuff:
 	ret
 
 PrintString:
-	; Input:
-	;  RAX -> address to print out
-	;  RBX -> length of address to print
-	; Output:
-	;  Prints eax to console
-	
 	; Push everything to stack because of the syscall
 	push RCX					; Add value of RCX to stack
 	push RDX					; Add value of RDX to stack
 	push RDI					; Add value of RDI to stack
 	push R8						; Add value of R8 to stack
-	push R9						; Add value of R9 to stack
-	mov RSI,RAX					; input to correct register
-	mov RDX,RBX					; lenght to correct register
+	push R10					; Add value of R10 to stack
+
+	mov RSI,RAX					; Write the user input to the RSI register					
+	mov RDX,RBX					; Move the length to the RDX register
 	mov RAX, 1					; Set mode to output
 	mov RDI, 1					; Set mode to output
 	syscall						; Make syscall
 	
 	; Return the values to the old registers
-	pop R9						; Get value back of R9
+	pop R10						; Get value back of R10
 	pop R8						; Get value back of r8
 	pop RDI						; Get value back of RDI
 	pop RDX						; Get value back of RDX
 	pop RCX						; Get value back of Rcs
-	
 	ret
 
-NotEqual:						; standard case
-	sub RDX, 32h				; remove 32h from ascii vALue of read character, this helps keeping the translation table smaller
-	mov CL, byte [Table+RDX]	; use the ascii value -50 as offset to get the 5bit value to CL
-	add RDI, RCX				; add RCX to RDI, had an issue with keeping the result in RCX, adding solved this
+NotEqual:						
+	sub RDX, 32H				; Subtract 50 to get the correct letter or number 
+	mov CL, byte [Table+RDX]	; Move the calculatet char into the CL register
+	add RDI, RCX				; Add the RCX content to the RDI register
 
 IsEqual:
-	inc RSI						; increase counter for read bytes (will continue when 8)
-	shl RDI, 5					; shift 5 bits left
-	cmp RSI, 8					; continue translating if 8 bytes are read
-	je Translate				; If RSI is 8 jump to the Translate function
-	
-	jmp Read					; continue reading buffer
+	inc RSI						; Increment the counter by 1
+	shl RDI, 5					; Shift left by 5 bits
+	cmp RSI, 8					; Compare the counter with 8 
+	je Translate				; If RSI is equal to 8 jump to 
+	jmp Read					; Else jump to Read
 	
 Translate:
-	shr RDI, 5					; because of loop in Read we shift one too many, shift it back
-	mov R9, RDI					; keep a backup of our shifted 5 bit pairs in R9 (40 bits together)
+	shr RDI, 5					; Shift the RDI by 5 bytes
+	mov R10, RDI				; Move the shifted bits in the R10 
 	xor RDI, RDI				; Reset the RDI register
 	xor RCX, RCX				; Reset the RCX register
 	xor RDX, RDX				; Reset the RDX register
-	mov RDI, 0					; RDI holds the value where to stop printing
-	mov CL, 40					; RCX holds the value how much shifting is needed to get 8 bits to print
+	mov RDI, 0					; Set RDI to 0
+	mov CL, 28H					; 
 	
-	cmp R8, 6					; different cases for = occurences
-	je Equal6					; go to according case
+	cmp R8, 6											
+	je Equal6					
 	cmp R8, 4
 	je Equal4
 	cmp R8, 3
 	je Equal3
 	cmp R8, 1
 	je Equal1
-	jmp Print					; default case is no =
+	jmp Print					
 	
+Equal6:							
+	add RDI, 8					
 	
-Equal6:							; print 1 byte
-	add RDI, 8					; 6 = has to stop after 1 printed byte -> need 32
+Equal4:							
+	add RDI, 8					
 	
-Equal4:							; print 2 bytes
-	add RDI, 8					; 4 = has to stop after 2 printed bytes -> need 24
+Equal3:							
+	add RDI, 8					
 	
-Equal3:							; print 3 bytes
-	add RDI, 8					; 3 = has to stop after 3 printed bytes -> 16
+Equal1:							
+	add RDI, 8					
 	
-Equal1:							; print 4 bytes
-	add RDI, 8					; 1 = has to stop after 4 printed bytes -> 8
-	
-Print:							; print 5 bytes
-	mov RDX, R9					; get the backup in RDX to work with
-	sub CL, 8					; remove 8 from RCX for the correct shifting
-	shr RDX, CL					; shift the first 8 bit
-	mov byte [Char], DL			; put it in Char
-	mov RAX, Char				; and print Char
-	mov RBX, RESULT				; length of Char
-	call PrintString			; print to console
-	cmp RCX, RDI				; compare RCX with RDI (either 0 or set by amount of = above)
-	jne Print					; print next if no match
+; Method to print the string we converted
+Print:
+	mov RDX, R10							
+	sub CL, 8					
+	shr RDX, CL					
+	mov byte [Char], DL			
+	mov RAX, Char				
+	mov RBX, RESULT				; Set the output to print
+	call PrintString			; Print out the string in the console
+	cmp RCX, RDI				; Compare the length of the Buff with the RCX
+	jne Print					; If not jump to the Print function
 	xor RCX, RCX				; Reset register RCX
 	xor RDX, RDX				; Reset register RDX
 	xor RSI, RSI				; Reset register RSI
 	xor RDI, RDI				; Reset register RDI
 	xor R8, R8					; Reset register R8
-	jmp Read					; Jump back to the Read function
+	jmp Read					
 	
 ; Clean exit of the program
 Exit:
-	mov RAX, 60					; Make clean exit (syscall)
+	mov RAX, 3CH				; Make clean exit (syscall)
 	mov RDI, 0					; Make clean exit
 	syscall
